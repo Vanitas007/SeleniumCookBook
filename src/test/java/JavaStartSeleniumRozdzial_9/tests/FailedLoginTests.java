44b28dca6fd2917cb0074412a20576294af3d434
Vanitas007 <piotr.zajac07@gmail.com>
2022-03-31 21:25:02 +0200

    Wydzielenie testów do osobnych klas. Przeniesienie duplikowanego kodu do klasy TestBase i wykorzystanie jej dziedziczeniem w klasach testowych
    Page Object Pattern - podsumowanie
    Zastosowanie zasady DRY z ang. Do Not Repeat Yourself

---
+++ src/test/java/JavaStartSeleniumRozdzial_9/tests/FailedLoginTests.java
@@ -0,0 +1,31 @@
+package JavaStartSeleniumRozdzial_9.tests;
+
+import JavaStartSeleniumRozdzial_9.page.objects.LandingPage;
+import JavaStartSeleniumRozdzial_9.page.objects.LoginPage;
+import JavaStartSeleniumRozdzial_9.page.objects.TopMenuPage;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class FailedLoginTests extends TestBase {
+
+    @Test
+    public void asUserTryToLogInWithIncorrectLoginAndPassword() {
+
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.clickOnEnterStoreLink();
+
+        TopMenuPage topMenuPage = new TopMenuPage(driver);
+        topMenuPage.clickOnSignInLink();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.typeIntoUserNameField("NotExistingLogin");
+        loginPage.typeIntoPasswordField("NotProperPassword");
+        loginPage.clickOnLoginButton();
+        String warningMessage = loginPage.getWarningMessage();
+
+        assertEquals(warningMessage, "Invalid username or password. Signon failed.");
+    }
+
+}
+
