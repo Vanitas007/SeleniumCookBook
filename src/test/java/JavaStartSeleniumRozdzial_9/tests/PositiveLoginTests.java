92085e5f4b45a795185c03d3ea41a8f7d29f45db
Vanitas007 <piotr.zajac07@gmail.com>
2022-04-10 13:42:46 +0200

    Temat_9 Relatywne URL
    Nowa klasa z ApplicationURLs ze stałymi z linkami do różnych miejsc.
    Implementacja linków w testach, skracając je i nie powielejąc tych samych przejść w wielu testach.

--- src/test/java/JavaStartSeleniumRozdzial_9/tests/PositiveLoginTests.java
+++ src/test/java/JavaStartSeleniumRozdzial_9/tests/PositiveLoginTests.java
@@ -1,14 +1,18 @@
 package JavaStartSeleniumRozdzial_9.tests;
 
+import JavaStartSeleniumRozdzial_9.driver.DriverUtils;
 import JavaStartSeleniumRozdzial_9.page.objects.LandingPage;
 import org.testng.annotations.Test;
 
+import static JavaStartSeleniumRozdzial_9.navigation.ApplicationURLs.LOGIN_URL;
 import static org.testng.Assert.assertTrue;
 
 public class PositiveLoginTests extends TestBase {
 
     @Test
     public void asUserLoginUsingValidLoginAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
+
         LandingPage landingPage = new LandingPage();
         boolean isBannerAfterLoginDisplayed = landingPage
                 .clickOnEnterStoreLink()
