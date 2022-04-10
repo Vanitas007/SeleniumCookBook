63ab967b4411bd0826b7000ce5790711efa9d21d
Vanitas007 <piotr.zajac07@gmail.com>
2022-04-10 21:05:22 +0200

    Podstawowa konfiguracja testów z pliku properties
    Aby nasz framework mógł skutecznie uruchamiać testy na innych maszynach, potrzebujemy mieć możliwość konfiguracji frameworka ze względu na różne konfiguracje oraz środowiska uruchomieniowe.
    Podsumowując, pliki properties w łatwy sposób umożliwiają nam szybką i sprawną konfigurację, bez konieczności modyfikacji kodu.

--- src/test/java/JavaStartSeleniumRozdzial_9/driver/DriverManager.java
+++ src/test/java/JavaStartSeleniumRozdzial_9/driver/DriverManager.java
@@ -1,27 +1,29 @@
 package JavaStartSeleniumRozdzial_9.driver;
 
+import JavaStartSeleniumRozdzial_9.configuration.LocalWebDriverProperties;
 import JavaStartSeleniumRozdzial_9.driver.manager.BrowserFactory;
 import JavaStartSeleniumRozdzial_9.driver.manager.BrowserType;
 import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
 
-    private static final BrowserType BROWSER_TYPE = BrowserType.CHROME;
     private static WebDriver driver;
 
     private DriverManager() {
     }
 
     public static WebDriver getWebDriver() {
+
         if (driver == null) {
-            driver = BrowserFactory.getBrowser(BROWSER_TYPE);
+            driver = BrowserFactory.getBrowser(LocalWebDriverProperties.getLocalBrowser());
         }
+
         return driver;
     }
 
     public static void disposeDriver() {
         driver.close();
-        if (!BROWSER_TYPE.equals(BrowserType.FIREFOX)) {
+        if (!LocalWebDriverProperties.getLocalBrowser().equals(BrowserType.FIREFOX)){
             driver.quit();
         }
         driver = null;
